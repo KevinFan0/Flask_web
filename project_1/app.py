22404ea629f4dce77a271ace3aff0468de21da43
樊冬 <fandong@MrFansdeMacBook-Pro.local>
2018-05-02 23:37:30 +0800

    0502

--- project_1/app.py
+++ project_1/app.py
@@ -1,3 +1,12 @@
-     #from flask import flask
+from flask import Flask
+app=Flask(__name__)
+@app.route('/')
+def index():
+    return '<h1>hello world!</h1>'
 
-print("hello world")
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello,%s!</h1>' % name
+
+if __name__ == '__main__':
+    app.run(debug=True)
