84a9b163103195f5334f17720e0accef3443b7ba
樊冬 <fandong@MrFansdeMacBook-Pro.local>
2018-05-05 23:34:57 +0800

    0505

--- project_1/app.py
+++ project_1/app.py
@@ -1,8 +1,22 @@
-from flask import Flask
+from flask import Flask,request
+from flask import current_app
+from flask import make_response
+
 app=Flask(__name__)
+app_ctx=app.app_context() #程序上下文
+app_ctx.push()
+
+
+# @app.route('/')
+# def index():
+#     user_agent=request.headers.get('User-Agent')
+#     return '<p>Your browser is %s</p>' %user_agent
+
 @app.route('/')
 def index():
-    return '<h1>hello world!</h1>'
+    respons=make_response('<h1>This document carries a cookie!</h1>')
+    respons.set_cookie('answer','42')
+    return respons
 
 @app.route('/user/<name>')
 def user(name):
