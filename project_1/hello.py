ea1ab4ed6d828e27ef4b9467f1aea13d8cd86c49
fandong <fandong@ppdai.com>
2018-05-17 19:40:52 +0800

    templates

--- project_1/hello.py
+++ project_1/hello.py
@@ -1,17 +1,31 @@
 #! /usr/bin/env python
 # coding=utf-8
 from flask import Flask,request,render_template
-from flask.ext.bootstrap import Bootstrap
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
 
-app=Flask(__name__)
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+app.config['SECRET_KEY'] = 'hard to guess string'       #设置密钥
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
-    return render_template('user.html',name=name)
+    return render_template('user.html', name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
+
 
 if __name__ == '__main__':
     app.run(debug=True)
